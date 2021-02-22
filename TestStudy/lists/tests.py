6fea0113f41d3eff4a206c4ec6b0739767bc1e7f
hyeseong <hystom38@gmail.com>
2021-02-22 21:03:46 +0900

    21/02/22
    21/02/22

--- TestStudy/lists/tests.py
+++ TestStudy/lists/tests.py
@@ -6,7 +6,8 @@ from django.urls import resolve
 from .views import home_page
 from .models import Item
 
-class HomePAgeTest(TestCase):
+
+class HomePageTest(TestCase):
 
     # def test_root_url_resolves_to_home_page_view(self):
     #     found = resolve('/')
@@ -19,18 +20,19 @@ class HomePAgeTest(TestCase):
     #     self.assertEqual(response.content.decode(), expected_html)
 
     def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
+        request = HttpRequest();
         request.method = 'POST'
         request.POST['item_text'] = '신규 작업 아이템'
 
         response = home_page(request)
 
-        self.assertIn('신규 작업 아이템', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': '신규 작업 아이템'}
-        )
-        # self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class ItemModelTest(TestCase):
--- TestStudy/lists/views.py
+++ TestStudy/lists/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
+
+from .models import Item
 
 
 @csrf_exempt
 def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
+
     })
